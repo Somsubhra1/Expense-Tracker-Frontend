1812e2043b0870ed7286c69d4703ca3b48e0d08c
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-15 11:14:33 +0530

    changed class to className and removed ids

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -4,10 +4,10 @@ export const TransactionList = () => {
   return (
     <>
       <h3>History</h3>
-      <ul id="list" class="list">
-        <li class="minus">
+      <ul className="list">
+        <li className="minus">
           Cash <span>-$400</span>
-          <button class="delete-btn">x</button>
+          <button className="delete-btn">x</button>
         </li>
       </ul>
     </>
