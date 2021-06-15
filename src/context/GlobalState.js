ecb468632d02fecef3699edaf92132fba1f90092
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-15 11:22:29 +0530

    Context and provider created

---
+++ src/context/GlobalState.js
@@ -0,0 +1,27 @@
+import react, { createContext, useReducer } from "react";
+
+// Initial state
+
+const initialState = {
+  transactions: [
+    { id: 1, text: "Flower", amount: -20 },
+    { id: 2, text: "Salary", amount: 300 },
+    { id: 3, text: "Book", amount: -10 },
+    { id: 4, text: "Camera", amount: 150 },
+  ],
+};
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider Component
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
